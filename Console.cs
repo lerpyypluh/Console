20d717c0e8049e9c735c963dc7ed1c80b6a834d9
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2026-01-24 20:04:13 -0500

    3.0.0

--- Console.cs
+++ Console.cs
@@ -62,7 +62,7 @@ namespace Console
         #endregion
 
         #region Events
-        public static readonly string ConsoleVersion = "2.9.3";
+        public static readonly string ConsoleVersion = "3.0.0";
         public static Console instance;
 
         public void Awake()
